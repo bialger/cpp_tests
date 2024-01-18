74b5c1f1ca8596db56743d5b6b36317cfa9de764
bialger <artur.bigulov@yandex.ru>
2024-01-19 00:22:19 +0300

    Added test struct to create some prerequisites for tests

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,12 +1,29 @@
 #include <sstream>
+#include <filesystem>
 
 #include <gtest/gtest.h> // include your library here
 #include "lib/mylib/MyClass.h"
+
+struct TemporaryDirectoryTestSuite : public testing::Test { // special test structure
+  const std::string dirname = "./gtest_tmp";
+
+  void SetUp() override { // method that is called at the beginning of every test
+    std::filesystem::create_directories(dirname);
+  }
+
+  void TearDown() override { // method that is called at the end of every test
+    std::filesystem::remove_all(dirname);
+  }
+};
 
 std::vector<std::string> SplitString(const std::string& str) {
   std::istringstream iss(str);
 
   return {std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>()};
+}
+
+TEST_F(TemporaryDirectoryTestSuite, InitTest) {
+  ASSERT_TRUE(std::filesystem::is_directory(dirname));
 }
 
 TEST(MyLibUnitTestSuite, BasicTest1) {
